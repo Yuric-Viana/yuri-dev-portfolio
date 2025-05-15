11ee5e6bf460fb4093c9d66f389d349a49a39dd5
Yuri Viana <ycviana@sga.pucminas.br>
2025-05-15 19:29:32 -0300

    feat: main para mobile finalizado

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const menuHamburguer = document.querySelector('.cabecalho-top__menu');
 const listaMenu = document.querySelector('.cabecalho-navegacao');
+const contentHidden = document.querySelector('.hidden');
+
+let ativo = true;
 
 menuHamburguer.addEventListener('click', () => {
     listaMenu.classList.toggle('ativo');
+
+    ativo = !ativo;
+
+    contentHidden.style.opacity = ativo ? 1 : 0;
 })
