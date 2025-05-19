c6da81e952b255fb013e99c8492111b7825d780d
Yuri Viana <ycviana@sga.pucminas.br>
2025-05-19 10:20:37 -0300

    feat: finalizar página de contato responsiva

--- scripts/contato.js
+++
@@ -1,9 +0,0 @@
-import { navBar } from "../script.js";
-
-const inputs = document.querySelectorAll('.contato-form__item--text');
-
-inputs.forEach(input => {
-    input.addEventListener('focus', () => {
-        input.classList.add('ativo');
-    });
-});
