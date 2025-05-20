ae00e285a99519d894ef97734a1dd9ca6a0ea618
Yuri Viana <ycviana@sga.pucminas.br>
2025-05-20 09:17:07 -0300

    feat: finalizar portfólio

--- scripts/contato.js
+++ scripts/contato.js
@@ -3,7 +3,9 @@ import { navBar } from "../script.js";
 const inputText = document.querySelectorAll('.contato-form__item--text');
 const enviarForm = document.getElementById('botao-enviar');
 
-enviarForm.addEventListener('click', () => {
+enviarForm.addEventListener('click', (e) => {
+    e.preventDefault();
+
     inputText.forEach(elemento => {
         elemento.textContent = "";
     })
