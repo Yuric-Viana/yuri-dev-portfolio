705ad2700c9b8ffe3bdccf02f9f529c80e548b4a
Yuri Viana <ycviana@sga.pucminas.br>
2025-05-23 11:03:35 -0300

    feat: atualizar icones header com icones e efeito

--- script.js
+++ script.js
@@ -2,6 +2,40 @@ const menuHamburguer = document.querySelector('.cabecalho-top__menu');
 const listaMenu = document.querySelector('.cabecalho-navegacao');
 const contentHidden = document.querySelectorAll('.hidden');
 const link = document.querySelectorAll('.cabecalho-navegacao__list--link');
+const liItem = document.querySelectorAll('.navegacao__list--item');
+const liImg = document.querySelector('.list--item__maleta');
+
+liItem.forEach(item => {
+    item.addEventListener('mouseover', (e) => {
+        let itemEscolhido = e.target.textContent;
+        let img = item.querySelector('img');
+
+        if(itemEscolhido.includes('Sobre')) {
+            if(img) img.src = 'assets/maleta.png'
+        
+        } else if(itemEscolhido.includes('Certificados')) {
+            if(img) img.src = 'assets/diploma-svgrepo-com.svg';
+
+        } else if(itemEscolhido.includes('Contato')) {
+            if (img) img.src = 'assets/contato-boneco-laranja.png'
+        }
+    })
+
+    item.addEventListener('mouseleave', (e) => {
+        let img = item.querySelector('img');
+
+        if(item.classList.contains('maleta')) {
+            if(img) img.src = 'assets/maleta-de-negocios-branca-transparente.png';
+ 
+        } else if(item.classList.contains('diploma')) {
+            if(img) img.src = 'assets/diploma-branco-removebg-preview.png';
+
+        } else if(item.classList.contains('contato--text')) {
+            if (img) img.src = 'assets/contato-boneco-branco-removebg-preview.png';
+        }
+    })
+})
+
 
 link.forEach(effect => {
     effect.addEventListener('click', (e) => {
--- scripts/sobre.js
+++ scripts/sobre.js
@@ -1,5 +1,67 @@
 import { navBar } from "../script.js";
 
+const ulSugestoes = document.querySelector('.main-sugestoes__list');
+const btnEnviar = document.querySelector('.main-sugestoes__btn');
+const inputList = document.querySelector('.main-sugestoes__input');
+const formLista = document.querySelector('.main-sugestoes__form');
+
+const sugestao = JSON.parse(localStorage.getItem('sugestao')) || [];
+
+function salvarItem() {
+    localStorage.setItem('sugestao', JSON.stringify(sugestao));
+}
+
+function criarSugestao(descricao) {
+    const listaItem = document.createElement('li');
+    listaItem.classList.add('sugestoes__list--item');
+
+    const paragrafo = document.createElement('p');
+    paragrafo.textContent = descricao;
+    
+    const icons = document.createElement('span');
+    icons.classList.add('list--item__icons');
+
+    const imgDelete = document.createElement('img');
+    imgDelete.id = 'delete';
+    imgDelete.src = 'assets/delete.png'; 
+
+    const imgEdite = document.createElement('img');
+    imgEdite.src = 'assets/edit.png';
+
+    icons.append(imgDelete);
+    icons.append(imgEdite);
+
+    listaItem.append(paragrafo);
+    listaItem.append(icons);
+
+    return listaItem;
+}
+
+formLista.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(inputList.value == '') {
+        alert('Deve ser inserido uma sugestão!');
+        return;
+    }
+
+    const sugestaoInformada = {
+        descricao: inputList.value
+    }
+
+    sugestao.push(sugestaoInformada);
+
+    const criarItemSugestao = criarSugestao(sugestaoInformada.descricao);
+    ulSugestoes.append(criarItemSugestao);
+
+    salvarItem();
+})
+
+sugestao.forEach(item => {
+    const elementoSugest = criarSugestao(item.descricao);
+    ulSugestoes.append(elementoSugest);
+})
+
 let divSlide = document.querySelectorAll('.main-btn');
 let cont = 1;
 
